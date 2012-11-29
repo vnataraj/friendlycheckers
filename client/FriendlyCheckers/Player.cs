84279f924af1a143bf5ffc1d922d39919c004977
Caleb Rouleau <calebrouleau@gmail.com>
2012-11-29 15:20:30 -0500

    moved player class to its own file

---
+++ client/FriendlyCheckers/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace FriendlyCheckers {
+    public class Player {
+        private PieceColor myColor;
+        private String myName;
+        public Player(String name, PieceColor color) {
+            this.myName = name;
+            this.myColor = color;
+        }
+        public Player(String name) : this(name, PieceColor.BLACK) { }
+        public Player() : this("Player", PieceColor.BLACK) { }
+        public void setName(String name) { this.myName = name; }
+        public void setColor(PieceColor color) { this.myColor = color; }
+        public String getName() { return myName; }
+        public PieceColor getColor() { return myColor; }
+    }
+}
