037cbd80d5d9438e2ed98de19a0e83cf05de2a61
Joseph Klemen <jmklemen@gmail.com>
2012-11-30 20:32:41 -0500

    Combined UserGame.cs with DataHandler.cs

--- client/FriendlyCheckers/DataHandler.cs
+++ client/FriendlyCheckers/DataHandler.cs
@@ -49,4 +49,32 @@ namespace FriendlyCheckers
                 result += SR.ReadLine();
         }
 	}
+    public class UserGame
+    {
+        int gameID;
+        int matchID;
+        string username;
+
+        public UserGame(int gameID, int matchID, string username)
+        {
+            this.gameID = gameID;
+            this.matchID = matchID;
+            this.username = username;
+        }
+
+        public int getGameID()
+        {
+            return gameID;
+        }
+
+        public int getMatchID()
+        {
+            return matchID;
+        }
+
+        public string getUsername()
+        {
+            return username;
+        }
+    }
 }
